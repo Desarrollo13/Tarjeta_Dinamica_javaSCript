52624c07bacbb6405637ed43c851b40d92837ff2
cristian <cristianmesa13@gmail.com>
2020-06-10 20:14:48 -0300

    se agrego el js termindo

--- Formulario Dinámico_Tarjeta de Crédito  HTML, CSS y Javascript/js/main.js
+++ Formulario Dinámico_Tarjeta de Crédito  HTML, CSS y Javascript/js/main.js
@@ -1,9 +1,11 @@
 //aca solo trabajamos para hacer girar la tarjeta
 const tarjeta = document.querySelector('#tarjeta'),
-    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario');
+    btnAbrirFormulario = document.querySelector('#btn-abrir-formulario'),
+    formulario = document.querySelector('#formulario-tarjeta');
 tarjeta.addEventListener('click', () => {
     tarjeta.classList.toggle('active');
 });
 btnAbrirFormulario.addEventListener('click', () => {
     btnAbrirFormulario.classList.toggle('active');
+    formulario.classList.toggle('active');
 });
